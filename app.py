9a48095b7590b667ad33d5bdd523e3559001f6d1
Daman Heer <daman.heer0@gmail.com>
2020-06-25 21:39:52 -0700

    Update 6/25

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
 import numpy as np
 
 
-st.title('STEM Career Project')
-st.header('Tech jobs and salaries across the United States')
 
-st.markdown('This is project was originally submitted as ')
+st.title('STEM Career Project Exploration')
+st.header('Tech jobs and Salaries across the United States')
+
+st.markdown(
+    'This is Web App is a modification of an assignment submitted as a final project for *COGS 108: Data Science in Practice* taught in Spring 2020.'
+    + ' The original notebook repo can be viewed [here](https://github.com/damehero/COGS108_Repo/blob/master/FinalProject_group62.ipynb).'
+)
+st.markdown('This page is meant to serve as an interactive and visual walkthrough of the original project along with additional findings.')
+
+st.header("'Software Engineering' jobs scraped from Glassdoor.com")
+df = pd.read_csv('swe.csv')
+st.dataframe(df.head())
+
 st.latex('a^2 + b^2 = c^2')
 
 activities = ["About", "Github"]
