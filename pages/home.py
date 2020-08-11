541011d69f6a600b1d0959926c03c65e2b5d4efc
Daman Heer <daman.heer0@gmail.com>
2020-08-10 19:22:12 -0700

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -7,7 +7,7 @@ def write():
     st.header('Tech jobs and Salaries across the United States')
 
     st.markdown(
-        '''This is Web App is a modified expansion on an assignment orignially submitted as a final project for *COGS 108: Data Science in Practice*, 
+        '''This Web App is a modified expansion on an assignment orignially submitted as a final project for *COGS 108: Data Science in Practice*, 
         taught remotely at UC San Diego in Spring 2020. The old repository and data can be viewed [here](https://github.com/damehero/COGS108_Repo/blob/master/FinalProject_group62.ipynb).'''
     )
     st.markdown(
